b09a64fa7993449c886e2c62c4fce4eb80dd1da2
Cristian Petra <cristian@artep.eu>
2020-09-07 12:02:23 +0300

    Init commit

---
+++ CPAlertController/CPAlertController.h
@@ -0,0 +1,19 @@
+//
+//  CPAlertController.h
+//  CPAlertController
+//
+//  Created by Cristian Petra on 07/09/2020.
+//  Copyright © 2020 artep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CPAlertController.
+FOUNDATION_EXPORT double CPAlertControllerVersionNumber;
+
+//! Project version string for CPAlertController.
+FOUNDATION_EXPORT const unsigned char CPAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CPAlertController/PublicHeader.h>
+
+
